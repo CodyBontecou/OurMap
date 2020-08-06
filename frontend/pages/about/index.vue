2fed3c89662c23f180da18f75def56bf4aa793d0
Cody Bontecou <bontecouc@gmail.com>
2020-08-06 18:50:25 -0500

    Container
    Moved some of the container code into a Tailwindcss @apply.

---
+++ frontend/pages/about/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <div>
+      <Logo />
+      <h1 class="title block font-light">
+        Our Map
+      </h1>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style>
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
