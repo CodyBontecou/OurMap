6a01de93c8ef45587c287e9f66a2f08058954294
Cody Bontecou <bontecouc@gmail.com>
2020-08-07 19:11:52 -0500

    vars → let/const.
    Also renamed anything `Home` related to UserLocation

--- frontend/components/Map.vue
+++ frontend/components/Map.vue
@@ -59,15 +59,15 @@ export default {
 
     // custom zoom bar control that includes a Zoom Home function
     let that = this;
-    L.Control.zoomHome = L.Control.extend({
+    L.Control.ZoomUserLocation = L.Control.extend({
       options: {
         position: "bottomright",
         zoomInText: "<i class=\"material-icons text-sm align-middle\">add</i>",
         zoomInTitle: "Zoom in",
         zoomOutText: "<i class=\"material-icons text-sm align-middle\">remove</i>",
         zoomOutTitle: "Zoom out",
-        zoomHomeText: "<i class=\"material-icons text-sm align-middle\">gps_fixed</i>",
-        zoomHomeTitle: "Zoom home"
+        zoomUserLocationText: "<i class=\"material-icons text-sm align-middle\">gps_fixed</i>",
+        zoomUserLocationTitle: "Zoom home"
       },
 
       onAdd: function(map) {
@@ -75,8 +75,8 @@ export default {
           container = L.DomUtil.create("div", controlName + " leaflet-bar"),
           options = this.options;
 
-        this._zoomHomeButton = this._createButton(options.zoomHomeText, options.zoomHomeTitle,
-          controlName + "-home", container, this._zoomHome);
+        this._zoomUserLocationButton = this._createButton(options.zoomUserLocationText, options.zoomUserLocationTitle,
+          controlName + "-home", container, this._zoomUserLocation);
         this._zoomInButton = this._createButton(options.zoomInText, options.zoomInTitle,
           controlName + "-in", container, this._zoomIn);
         this._zoomOutButton = this._createButton(options.zoomOutText, options.zoomOutTitle,
@@ -100,10 +100,10 @@ export default {
         this._map.zoomOut(e.shiftKey ? 3 : 1);
       },
 
-      // TODO: 1. Change icon color when success,
+      // TODO: 1. Change icon color when success
       // TODO: 2. Disable the icon and setView logic if location was designed
       // TODO: 3. Neutral case where user hasn't given permission yet.
-      _zoomHome: function() {
+      _zoomUserLocation: function() {
         if ("geolocation" in navigator) {
           // check if geolocation is supported/enabled on current browser
           navigator.geolocation.getCurrentPosition(
@@ -113,9 +113,9 @@ export default {
 
               if (that.userLocationMarker !== null) {
                 that.userLocationMarker.remove();
-                console.log("removed");
               }
 
+              // TODO: 1. Change marker to be more explicit and unique.
               that.userLocationMarker = L.marker([lat, lng]).addTo(map);
 
               map.setView(
@@ -164,20 +164,18 @@ export default {
         if (map._zoom === map.getMaxZoom()) {
           L.DomUtil.addClass(this._zoomInButton, className);
         }
-        L.DomUtil.addClass(this._zoomHomeButton, "text-blue-500");
       }
     });
     // add the new control to the map
-    const zoomHome = new L.Control.zoomHome();
-    zoomHome.addTo(map);
+    const zoomUserLocation = new L.Control.ZoomUserLocation();
+    zoomUserLocation.addTo(map);
 
     // URL Hashing
-    var HAS_HASHCHANGE = (function() {
-      var doc_mode = window.documentMode;
+    let HAS_HASHCHANGE = (function() {
+      const doc_mode = window.documentMode;
       return ("onhashchange" in window) &&
         (doc_mode === undefined || doc_mode > 7);
     })();
-
     L.Hash = function(map) {
       this.onHashChange = L.Util.bind(this.onHashChange, this);
 
@@ -185,14 +183,13 @@ export default {
         this.init(map);
       }
     };
-
     L.Hash.parseHash = function(hash) {
       if (hash.indexOf("#") === 0) {
         hash = hash.substr(1);
       }
-      var args = hash.split("/");
-      if (args.length == 3) {
-        var zoom = parseInt(args[0], 10),
+      let args = hash.split("/");
+      if (args.length === 3) {
+        let zoom = parseInt(args[0], 10),
           lat = parseFloat(args[1]),
           lon = parseFloat(args[2]);
         if (isNaN(zoom) || isNaN(lat) || isNaN(lon)) {
@@ -207,9 +204,8 @@ export default {
         return false;
       }
     };
-
     L.Hash.formatHash = function(map) {
-      var center = map.getCenter(),
+      const center = map.getCenter(),
         zoom = map.getZoom(),
         precision = Math.max(0, Math.ceil(Math.log(zoom) / Math.LN2));
 
@@ -217,112 +213,111 @@ export default {
         center.lat.toFixed(precision),
         center.lng.toFixed(precision)
       ].join("/");
-    },
+    };
+    L.Hash.prototype = {
+      map: null,
+      lastHash: null,
 
-      L.Hash.prototype = {
-        map: null,
-        lastHash: null,
+      parseHash: L.Hash.parseHash,
+      formatHash: L.Hash.formatHash,
 
-        parseHash: L.Hash.parseHash,
-        formatHash: L.Hash.formatHash,
+      init: function(map) {
+        this.map = map;
 
-        init: function(map) {
-          this.map = map;
+        // reset the hash
+        this.lastHash = null;
+        this.onHashChange();
 
-          // reset the hash
-          this.lastHash = null;
-          this.onHashChange();
-
-          if (!this.isListening) {
-            this.startListening();
-          }
-        },
-
-        removeFrom: function(map) {
-          if (this.changeTimeout) {
-            clearTimeout(this.changeTimeout);
-          }
-
-          if (this.isListening) {
-            this.stopListening();
-          }
-
-          this.map = null;
-        },
-
-        onMapMove: function() {
-          // bail if we're moving the map (updating from a hash),
-          // or if the map is not yet loaded
-
-          if (this.movingMap || !this.map._loaded) {
-            return false;
-          }
-
-          var hash = this.formatHash(this.map);
-          if (this.lastHash != hash) {
-            location.replace(hash);
-            this.lastHash = hash;
-          }
-        },
-
-        movingMap: false,
-        update: function() {
-          var hash = location.hash;
-          if (hash === this.lastHash) {
-            return;
-          }
-          var parsed = this.parseHash(hash);
-          if (parsed) {
-            this.movingMap = true;
-
-            this.map.setView(parsed.center, parsed.zoom);
-
-            this.movingMap = false;
-          } else {
-            this.onMapMove(this.map);
-          }
-        },
-
-        // defer hash change updates every 100ms
-        changeDefer: 100,
-        changeTimeout: null,
-        onHashChange: function() {
-          // throttle calls to update() so that they only happen every
-          // `changeDefer` ms
-          if (!this.changeTimeout) {
-            var that = this;
-            this.changeTimeout = setTimeout(function() {
-              that.update();
-              that.changeTimeout = null;
-            }, this.changeDefer);
-          }
-        },
-
-        isListening: false,
-        hashChangeInterval: null,
-        startListening: function() {
-          this.map.on("moveend", this.onMapMove, this);
-
-          if (HAS_HASHCHANGE) {
-            L.DomEvent.addListener(window, "hashchange", this.onHashChange);
-          } else {
-            clearInterval(this.hashChangeInterval);
-            this.hashChangeInterval = setInterval(this.onHashChange, 50);
-          }
-          this.isListening = true;
-        },
-
-        stopListening: function() {
-          this.map.off("moveend", this.onMapMove, this);
-
-          if (HAS_HASHCHANGE) {
-            L.DomEvent.removeListener(window, "hashchange", this.onHashChange);
-          } else {
-            clearInterval(this.hashChangeInterval);
-          }
-          this.isListening = false;
+        if (!this.isListening) {
+          this.startListening();
         }
-      };
+      },
+
+      removeFrom: function() {
+        if (this.changeTimeout) {
+          clearTimeout(this.changeTimeout);
+        }
+
+        if (this.isListening) {
+          this.stopListening();
+        }
+
+        this.map = null;
+      },
+
+      onMapMove: function() {
+        // bail if we're moving the map (updating from a hash),
+        // or if the map is not yet loaded
+
+        if (this.movingMap || !this.map._loaded) {
+          return false;
+        }
+
+        let hash = this.formatHash(this.map);
+        if (this.lastHash !== hash) {
+          location.replace(hash);
+          this.lastHash = hash;
+        }
+      },
+
+      movingMap: false,
+      update: function() {
+        const hash = location.hash;
+        if (hash === this.lastHash) {
+          return;
+        }
+        const parsed = this.parseHash(hash);
+        if (parsed) {
+          this.movingMap = true;
+
+          this.map.setView(parsed.center, parsed.zoom);
+
+          this.movingMap = false;
+        } else {
+          this.onMapMove(this.map);
+        }
+      },
+
+      // defer hash change updates every 100ms
+      changeDefer: 100,
+      changeTimeout: null,
+      onHashChange: function() {
+        // throttle calls to update() so that they only happen every
+        // `changeDefer` ms
+        if (!this.changeTimeout) {
+          const that = this;
+          this.changeTimeout = setTimeout(function() {
+            that.update();
+            that.changeTimeout = null;
+          }, this.changeDefer);
+        }
+      },
+
+      isListening: false,
+      hashChangeInterval: null,
+      startListening: function() {
+        this.map.on("moveend", this.onMapMove, this);
+
+        if (HAS_HASHCHANGE) {
+          L.DomEvent.addListener(window, "hashchange", this.onHashChange);
+        } else {
+          clearInterval(this.hashChangeInterval);
+          this.hashChangeInterval = setInterval(this.onHashChange, 50);
+        }
+        this.isListening = true;
+      },
+
+      stopListening: function() {
+        this.map.off("moveend", this.onMapMove, this);
+
+        if (HAS_HASHCHANGE) {
+          L.DomEvent.removeListener(window, "hashchange", this.onHashChange);
+        } else {
+          clearInterval(this.hashChangeInterval);
+        }
+        this.isListening = false;
+      }
+    };
     L.hash = function(map) {
       return new L.Hash(map);
     };
@@ -333,7 +328,7 @@ export default {
       this._hash.removeFrom();
     };
 
-    const hash = new L.Hash(map);
+    new L.Hash(map);
 
   }
 };
