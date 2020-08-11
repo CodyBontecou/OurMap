06292555b9e1a2f8aa2d714fce4e46ac50054d84
Cody Bontecou <bontecouc@gmail.com>
2020-08-11 12:53:05 -0500

    Cypress browser permission plugin to allow for geolocation setting to be set while testing.

--- frontend/cypress/integration/map.spec.js
+++ frontend/cypress/integration/map.spec.js
@@ -1,5 +1,38 @@
-describe('My First Test', () => {
-  it('Does not do much!', () => {
-    expect(true).to.equal(true)
+import { isPermissionAllowed } from 'cypress-browser-permissions';
+
+describe('Testing the Map component', () => {
+  before(() => {
+    cy.visit('http://localhost:3000/#3/39.81/-98.56')
   })
+
+  it("Location Permission be enabled", () => {
+    expect(isPermissionAllowed("geolocation")).to.be.true;
+  })
+
+  it("User Location Marker initially loaded", () => {
+    expect(isPermissionAllowed("geolocation")).to.be.true;
+    cy.get('.leaflet-marker-icon')
+      .should('exist')
+  })
+
+  // TODO: This could test more precisely its the users location (not just zoom level)
+  it('Clicking zoom-home button zooms to user location', () => {
+    cy.get('.gin-control-zoom-home')
+      .click()
+    cy.url().should('contain', '/#16/');
+  })
+
+  // TODO: Messing with the URL (zoom) seems to be breaking the test runner
+  it('Clicking zoom-out button zooms out', () => {
+    cy.get('.gin-control-zoom-out')
+      .click()
+    cy.url().should('contain', '/#15/');
+  })
+
+  it('Clicking zoom-in button zooms in', () => {
+    cy.get('.gin-control-zoom-in')
+      .click()
+    cy.url().should('contain', '/#16/');
+  })
+
 })
