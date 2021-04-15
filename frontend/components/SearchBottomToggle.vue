3cceced6ffb947651c1c5e5d4362493fc68fb853
Cody Bontecou <bontecouc@gmail.com>
2021-04-15 08:22:12 -1000

    eslint prettified on save

--- frontend/components/SearchBottomToggle.vue
+++ frontend/components/SearchBottomToggle.vue
@@ -1,7 +1,14 @@
 <template>
-  <button class="group flex items-center" style="height: 36px;" v-show="!searching">
+  <button
+    v-show="!searching"
+    class="group flex items-center"
+    style="height: 36px"
+  >
     <div class="flex">
-      <i class="pl-3 pr-5 material-icons text-xl text-gray-700 group-hover:text-gray-900">keyboard_arrow_down</i>
+      <i
+        class="pl-3 pr-5 material-icons text-xl text-gray-700 group-hover:text-gray-900"
+        >keyboard_arrow_down</i
+      >
       <div class="text-sm text-gray-700 group-hover:text-gray-900">
         See travel times, traffic, and nearby places.
       </div>
@@ -10,11 +17,11 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+  import { mapGetters } from 'vuex'
 
-export default {
-  computed: {
-    ...mapGetters({ searching: 'getSearching', })
-  },
-}
+  export default {
+    computed: {
+      ...mapGetters({ searching: 'getSearching' }),
+    },
+  }
 </script>
