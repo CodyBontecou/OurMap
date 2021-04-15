b7721fc2cdfff2fe29f8186cc4819223321caeb5
Cody Bontecou <bontecouc@gmail.com>
2021-04-15 08:21:35 -1000

    Hardcoding path to logo file

--- frontend/components/Logo.vue
+++ frontend/components/Logo.vue
@@ -1,16 +1,16 @@
 <template>
-  <img src="/logo.png" class="Logo" alt="OurMap Logo">
+  <img src="../static/logo.png" class="Logo" alt="OurMap Logo" />
 </template>
 
 <style>
-.Logo {
-  animation: 1s appear;
-  margin: auto;
-}
-
-@keyframes appear {
-  0% {
-    opacity: 0;
+  .Logo {
+    animation: 1s appear;
+    margin: auto;
   }
-}
+
+  @keyframes appear {
+    0% {
+      opacity: 0;
+    }
+  }
 </style>
