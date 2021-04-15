5587b2098fc57be08bcdf96564c4d895ffb20626
Cody Bontecou <bontecouc@gmail.com>
2021-04-15 08:24:14 -1000

    es-ES lang file added for i18n

---
+++ frontend/lang/es-ES.js
@@ -0,0 +1,74 @@
+export default {
+  // ****************************** Global ******************************
+  appName: 'Our Map',
+  menu: 'menu',
+  search: 'search',
+  directions: 'directions',
+
+  // ****************************** Search Input ******************************
+  searchInput: {
+    placeholder: 'search our map',
+  },
+
+  // ****************************** Map ******************************
+  map: {
+    location: 'location',
+    zoomIn: 'zoom in',
+    zoomOut: 'zoom out',
+  },
+
+  // ****************************** Apps ******************************
+  apps: {
+    name: 'apps',
+    apps: [
+      {
+        name: 'account',
+        img: {
+          src: 'cody.jpg',
+          alt: '',
+        },
+        to: 'account',
+      },
+      {
+        name: 'favorites',
+        img: {
+          src: 'logo.png',
+          alt: '',
+        },
+        to: '#',
+      },
+      {
+        name: 'blog',
+        img: {
+          src: 'logo.png',
+          alt: '',
+        },
+        to: '#',
+      },
+      {
+        name: 'contribute',
+        img: {
+          src: 'logo.png',
+          alt: '',
+        },
+        to: '#',
+      },
+      {
+        name: 'about',
+        img: {
+          src: 'logo.png',
+          alt: '',
+        },
+        to: 'about',
+      },
+      {
+        name: 'contact',
+        img: {
+          src: 'logo.png',
+          alt: '',
+        },
+        to: '#',
+      },
+    ],
+  },
+}
