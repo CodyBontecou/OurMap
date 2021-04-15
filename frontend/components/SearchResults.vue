f63d38228c5d7c80a6e8c7a7ef47c908024a6611
Cody Bontecou <bontecouc@gmail.com>
2021-04-15 08:23:43 -1000

    A bit of styling for SearchResults

--- frontend/components/SearchResults.vue
+++ frontend/components/SearchResults.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col z-1000" :class="{ 'py-2': results.length > 0 }">
-    <ul class="w-full autocomplete-results" style="overflow: auto;">
+    <ul class="w-full autocomplete-results" style="overflow: auto">
       <li
         v-for="(result, i) in results"
         :key="i"
@@ -8,7 +8,7 @@
         @click="emitFocus(result)"
       >
         <i class="pl-3 pr-5 material-icons text-xl text-gray-500">room</i>
-        <div class="text-sm text-gray-700">
+        <div class="text-sm text-gray-700 truncate">
           {{ result.name }}
         </div>
       </li>
